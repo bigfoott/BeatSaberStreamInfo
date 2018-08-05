2180c22f2018da75759b3adafd3c6fe11f0a7e51
bigfoot <8753187+bigfoott@users.noreply.github.com>
2018-08-05 14:47:25 -0400

    added combo, multiplier, and progress tracking

--- BeatSaberStreamInfo/Plugin.cs
+++ BeatSaberStreamInfo/Plugin.cs
@@ -15,8 +15,10 @@ namespace BeatSaberStreamInfo
         public string Version => "1.0";
 
         AudioTimeSyncController ats;
+        BeatmapData bmdata;
+        BeatmapObjectSpawnController spawncontroller;
 
-        private readonly string dir = Environment.CurrentDirectory;
+        private readonly string dir = Path.Combine(Environment.CurrentDirectory, "UserData/StreamInfo");
 
         string lastDuration;
 
@@ -28,18 +30,38 @@ namespace BeatSaberStreamInfo
         
         private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
         {
+            ats = Resources.FindObjectsOfTypeAll<AudioTimeSyncController>().FirstOrDefault();
+            //bmdata = Resources.FindObjectsOfTypeAll<BeatmapData>().FirstOrDefault();
+            spawncontroller = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>().FirstOrDefault();
+            
             var score = UnityEngine.Object.FindObjectOfType<ScoreController>();
             if (score != null)
             {
-                score.noteWasCutEvent += OnHit; 
+                score.comboDidChangeEvent += OnComboChange;
+                score.multiplierDidChangeEvent += OnMultiplierChange;
+                score.noteWasMissedEvent += OnNoteMiss;
+
+                File.WriteAllText(Path.Combine(dir, "Combo.txt"), "0");
+                File.WriteAllText(Path.Combine(dir, "Multiplier.txt"), "1x");
             }
-            
-            ats = Resources.FindObjectsOfTypeAll<AudioTimeSyncController>().FirstOrDefault();
+
         }
 
-        public void OnHit(NoteData data, NoteCutInfo cut, int h)
-        {
 
+        public void OnComboChange(int c)
+        {
+            File.WriteAllText(Path.Combine(dir, "Combo.txt"), "" + c);
+        }
+
+        public void OnMultiplierChange(int c, float f)
+        {
+            File.WriteAllText(Path.Combine(dir, "Multiplier.txt"), c + "x");
+        }
+
+        public void OnNoteMiss(NoteData data, int c)
+        {
+            File.WriteAllText(Path.Combine(dir, "Combo.txt"), "0");
+            File.WriteAllText(Path.Combine(dir, "Multiplier.txt"), "1x");
         }
 
         private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -69,14 +91,20 @@ namespace BeatSaberStreamInfo
                 string percent = ((ats.songTime / ats.songLength) * 100).ToString("N0");
                 string output = time + "/" + totaltime + " (" + percent + "%)";
                 
-                if (lastDuration == null || lastDuration != output)
+                if (lastDuration != output)
                 {
                     lastDuration = output;
-                    File.WriteAllText(Path.Combine(dir, "SongProgress.txt"), output);
+                    File.WriteAllText(Path.Combine(dir, "Progress.txt"), output);
                 }
             }
-            else
-                File.WriteAllText(Path.Combine(dir, "SongProgress.txt"), "");
+            else if (lastDuration != "")
+            {
+                lastDuration = "";
+                File.WriteAllText(Path.Combine(dir, "Progress.txt"), "");
+
+                File.WriteAllText(Path.Combine(dir, "Combo.txt"), "0");
+                File.WriteAllText(Path.Combine(dir, "Multiplier.txt"), "1x");
+            }
 
         }
 
